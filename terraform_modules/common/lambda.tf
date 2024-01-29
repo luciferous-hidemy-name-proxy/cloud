a5a69ee53181945f8cfcd04e1d99534d70a75c92
sinofseven <em.s.00001@gmail.com>
2024-01-30 01:04:49 +0900

    fix

--- terraform_modules/common/lambda.tf
+++ terraform_modules/common/lambda.tf
@@ -4,6 +4,7 @@ module "tmp_001" {
   function_identifier = "tmp_001"
   handler             = "index.handler"
   memory_size         = 128
+  layers = [data.aws_ssm_parameter.base_layer_arn.value]
 
   system_name = var.system_name
   role_arn    = aws_iam_role.tmp.arn
