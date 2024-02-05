32fef49aa8d1330100e498249602e6352a3d599d
sinofseven <em.s.00001@gmail.com>
2024-02-06 03:04:00 +0900

    fix pipe take 2

--- terraform_modules/common/pipe.tf
+++ terraform_modules/common/pipe.tf
@@ -3,7 +3,7 @@ resource "aws_pipes_pipe" "for_check" {
   target   = aws_sqs_queue.check_proxy.arn
   role_arn = aws_iam_role.pipe_for_check.arn
 
-  source_parameters = {
+  source_parameters {
     dynamodb_stream_parameters = {
       starting_position = "TRIM_HORIZON"
     }
