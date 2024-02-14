59a7d91588986ec408c31564cfbb4e57a5e013eb
sinofseven <em.s.00001@gmail.com>
2024-02-14 14:51:31 +0900

    add lambda function module

---
+++ terraform_modules/lambda_function/variables.tf
@@ -0,0 +1,65 @@
+variable "function_identifier" {
+  type     = string
+  nullable = false
+}
+
+variable "system_name" {
+  type     = string
+  nullable = false
+}
+
+variable "role_arn" {
+  type     = string
+  nullable = false
+}
+
+variable "runtime" {
+  type     = string
+  nullable = false
+  default  = "python3.12"
+}
+
+variable "handler" {
+  type     = string
+  nullable = false
+}
+
+variable "memory_size" {
+  type     = number
+  nullable = false
+  default  = 256
+}
+
+variable "timeout" {
+  type     = number
+  nullable = false
+  default  = 120
+}
+
+variable "layers" {
+  type     = list(string)
+  nullable = false
+  default  = []
+}
+
+variable "region" {
+  type     = string
+  nullable = false
+}
+
+variable "environment_variables" {
+  type     = map(string)
+  nullable = false
+  default  = {}
+}
+
+variable "alias" {
+  type     = string
+  nullable = false
+  default  = "alias"
+}
+
+variable "subscription_destination_lambda_arn" {
+  type     = string
+  nullable = false
+}
