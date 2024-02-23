dd89f3ed2b64e9cdc633655aec373ee9787600d5
sinofseven <em.s.00001@gmail.com>
2024-02-23 22:23:24 +0900

    rename queues

--- terraform_modules/common/sqs.tf
+++ terraform_modules/common/sqs.tf
@@ -1,15 +1,14 @@
 resource "aws_sqs_queue" "check_proxy_checker" {
+  name_prefix = "check_proxy_checker"
   visibility_timeout_seconds = 300
 }
 
 resource "aws_sqs_queue" "check_proxy_closer" {
+  name_prefix = "check_proxy_closer"
   visibility_timeout_seconds = 150
 }
 
 resource "aws_sqs_queue" "check_proxy_put_data" {
+  name_prefix = "check_proxy_put_data"
   visibility_timeout_seconds = 150
-}
-
-resource "aws_sqs_queue" "check_proxy" {
-  visibility_timeout_seconds = 300
 }
