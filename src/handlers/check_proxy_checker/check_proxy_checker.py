a667455d0dd4adbdfa60cd18c3a0e2a132df8e75
sinofseven <em.s.00001@gmail.com>
2024-02-25 15:24:27 +0900

    tmp

--- src/handlers/check_proxy_checker/check_proxy_checker.py
+++ src/handlers/check_proxy_checker/check_proxy_checker.py
@@ -1,8 +1,20 @@
+from dataclasses import dataclass
+
+from aws_lambda_powertools.utilities.data_classes import SQSEvent, event_source
 from common.logger import create_logger, logging_handler
+
+
+@dataclass(frozen=True)
+class ParsedEvent:
+    uuid: str
+    host: str
+    table_name: str
+
 
 logger = create_logger(__name__)
 
 
+@event_source(data_class=SQSEvent)
 @logging_handler(logger)
 def handler(event: dict, context):
     pass
