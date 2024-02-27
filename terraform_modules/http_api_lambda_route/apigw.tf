cea3c0648a7a48fc77854fddab66f7ce52115d3e
sinofseven <em.s.00001@gmail.com>
2024-02-27 17:46:52 +0900

    fix

--- terraform_modules/http_api_lambda_route/apigw.tf
+++ terraform_modules/http_api_lambda_route/apigw.tf
@@ -2,7 +2,7 @@ resource "aws_apigatewayv2_integration" "integration" {
   api_id                 = var.api_id
   integration_type       = "AWS_PROXY"
   integration_uri        = var.lambda_arn
-  payload_format_version = "2.0"
+  payload_format_version = "1.0"
 }
 
 resource "aws_apigatewayv2_route" "route" {
